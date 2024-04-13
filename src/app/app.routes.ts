d66d6ac743f978195fce1f40adbb8e92c7a32521
Guiliano Rangel <guiliano@gmail.com>
2024-04-12 21:57:15 -0300

    Criação das rotas para o componente tasks

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { TaskListComponent } from './tasks/task-list/task-list.component';
+import {TaskFormComponent} from "./tasks/task-form/task-form.component";
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: TaskListComponent },
+  { path: 'new', component: TaskFormComponent },
+  { path: 'edit/:id', component: TaskFormComponent }
+
+];
